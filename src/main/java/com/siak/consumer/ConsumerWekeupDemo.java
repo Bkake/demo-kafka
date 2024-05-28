48cd8796e45edfdb2c9746387a2e555a40fe4e1e
Bangaly KAKE <bangaly.kake-prestataire@docapost.fr>
2024-05-28 09:33:12 +0200

    adding grace full shutdown

--- src/main/java/com/siak/consumer/ConsumerWekeupDemo.java
+++ src/main/java/com/siak/consumer/ConsumerWekeupDemo.java
@@ -1,6 +1,5 @@
 package com.siak.consumer;
 
-import com.siak.handler.HandleRebalanced;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -10,72 +9,65 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
+import java.util.Collections;
 import java.util.Properties;
 
-import static java.util.Collections.singletonList;
 import static org.apache.kafka.clients.consumer.ConsumerConfig.AUTO_OFFSET_RESET_CONFIG;
 import static org.apache.kafka.clients.consumer.ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG;
-import static org.apache.kafka.clients.consumer.ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG;
 import static org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG;
 import static org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG;
 import static org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG;
 
-public class ConsumerRebalancedListenersDemo {
-    public  static final Logger logger = LoggerFactory.getLogger(ConsumerRebalancedListenersDemo.class);
+public class ConsumerWekeupDemo {
+    public  static final Logger logger = LoggerFactory.getLogger(ConsumerWekeupDemo.class);
 
-    @SuppressWarnings("InfiniteLoopStatement")
     public static void main(String[] args) {
-        logger.info( "I am a Kafka Consumer Rebalanced listeners !" );
+        logger.info( "I am a Kafka Consumer Wakeup!" );
 
         Properties properties = new Properties();
-        properties.put(BOOTSTRAP_SERVERS_CONFIG, "localhost:39092");
+        properties.put(BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
         properties.put(KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.put(VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.put(AUTO_OFFSET_RESET_CONFIG, "latest");
-        properties.put(ENABLE_AUTO_COMMIT_CONFIG, false);
-        properties.put(GROUP_ID_CONFIG, "Rebalanced");
+        properties.put(GROUP_ID_CONFIG, "lamaye-wakeup");
+
+        String topic = "wakeupTopic";
 
         KafkaConsumer<String,String> consumer = new KafkaConsumer<>(properties);
-        HandleRebalanced handleRebalanced = new HandleRebalanced(consumer);
 
         final Thread mainThread = Thread.currentThread();
 
+        // adding the shutdown hook
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             logger.info("Detected a shutdown, let's exit by calling consumer.wakeup()...");
             consumer.wakeup();
+
             try {
                 mainThread.join();
             } catch (InterruptedException e) {
                 logger.error(e.getMessage());
-                Thread.currentThread().interrupt();
             }
         }));
 
         try {
-            consumer.subscribe(singletonList("RebalancedTopic"), new HandleRebalanced(consumer));
+            consumer.subscribe(Collections.singletonList(topic));
             while (true) {
-                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-                for (ConsumerRecord<String, String> consumerRecord : records) {
-                    logger.info("Partition: {}, Key: {}, Value: {}, offset: {} ", consumerRecord.key(),
-                            consumerRecord.value(), consumerRecord.partition(), consumerRecord.offset());
+                ConsumerRecords<String, String> records =
+                        consumer.poll(Duration.ofMillis(100));
 
-                    handleRebalanced.addOffsetToTrack(consumerRecord.topic(),
-                            consumerRecord.partition(), consumerRecord.offset());
+                for (ConsumerRecord<String, String> record : records) {
+                    logger.info("Key: " + record.key() + ", Value: " + record.value());
+                    logger.info("Partition: " + record.partition() + ", Offset:" + record.offset());
                 }
-                consumer.commitAsync();
             }
         } catch (WakeupException e) {
-            // we ignore this as this is an expected exception when closing a consumer
+            logger.info("Wakeup exception");
         } catch (Exception e) {
-            logger.error("Unexpected exception", e);
+            logger.error(e.getMessage());
         } finally {
-            try {
-                consumer.commitSync(handleRebalanced.getCurrentOffsets());
-            } finally {
-                consumer.close();
-                logger.info("The consumer is now gracefully closed.");
-            }
+            consumer.close();
+            logger.info("The consumer is now gracefully closed.");
         }
+
     }
 }
-
--- src/main/java/com/siak/handler/HandleRebalanced.java
+++ src/main/java/com/siak/handler/HandleRebalanced.java
@@ -8,30 +8,41 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 
 public class HandleRebalanced implements ConsumerRebalanceListener {
     private  static final Logger logger =
             LoggerFactory.getLogger(HandleRebalanced.class);
+    private KafkaConsumer<String, String> consumer;
+    private Map<TopicPartition, OffsetAndMetadata> currentOffsets = new HashMap<>();
 
-    public KafkaConsumer<String,String> consumer;
-    public Map<TopicPartition, OffsetAndMetadata> currentOffsets;
-    public HandleRebalanced(KafkaConsumer<String,String> consumer,
-                            Map<TopicPartition, OffsetAndMetadata> currentOffsets) {
+    public HandleRebalanced(KafkaConsumer<String,String> consumer) {
         this.consumer = consumer;
-        this.currentOffsets = currentOffsets;
     }
+
+
     @Override
     public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
-        logger.info("Lost partitions in rebalanced. " +
-                "Committing current offsets: {}", currentOffsets);
+        logger.info("onPartitionsRevoked callback triggered");
+        logger.info("Committing offsets: {}", currentOffsets);
         consumer.commitSync(currentOffsets);
     }
 
     @Override
     public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
-        partitions.forEach(topicPartition -> logger.info("topic {}, partition {}",
-                topicPartition.topic(), topicPartition.partition()));
+        logger.info("onPartitionsAssigned callback triggered");
 
+    }
+
+    public void addOffsetToTrack(String topic, int partition, long offset) {
+        this.currentOffsets.put(
+                new TopicPartition(topic, partition),
+                new OffsetAndMetadata(offset+1, null));
+    }
+
+    // this is used when we shut down our consumer gracefully
+    public Map<TopicPartition, OffsetAndMetadata> getCurrentOffsets() {
+        return this.currentOffsets;
     }
 }
